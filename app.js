75e1efdcfb391093e3c310952d90d757f6b6f604
Eliott Lambert <eliott.lambert@konbini.com>
2019-03-07 17:25:00 +0100

    cloudTSS.js is now a class

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 "use strict";
-let RedditThreadFetcher = require("./class/reddit-api");
+const RedditThreadFetcher = require("./class/reddit-api");
+const CloudTTS = require("./class/cloud-tts");
 
-
+// Create video function
 async function buildVideo() {
     let reddit = new RedditThreadFetcher("AskReddit");
+    let cloudTTS = new CloudTTS();
+
     await reddit.buildContent();
+    await cloudTTS.testSynthetize();
 
     console.log(reddit.videoContent.comments.length);
 }
--- class/cloud-tts.js
+++ class/cloud-tts.js
@@ -2,29 +2,34 @@ const fs = require('fs');
 const util = require('util');
 const textToSpeech = require('@google-cloud/text-to-speech');
 
+class CloudTTS {
+    constructor () {
+        this.client = new textToSpeech.TextToSpeechClient();
+    }
+    
+    async testSynthetize () {
+        // The text to synthesize
+        const text = 'Hello, world!';
 
-// Creates a client
-const client = new textToSpeech.TextToSpeechClient();
+        // Construct the request
+        const request = {
+            input: {text: text},
+            // Select the language and SSML Voice Gender (optional)
+            voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+            // Select the type of audio encoding
+            audioConfig: {audioEncoding: 'MP3'},
+        };
 
-async function testSynthetize () {
-// The text to synthesize
-    const text = 'Hello, world!';
+        // Performs the Text-to-Speech request
+        const [response] = await this.client.synthesizeSpeech(request);
 
-// Construct the request
-    const request = {
-        input: {text: text},
-        // Select the language and SSML Voice Gender (optional)
-        voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
-        // Select the type of audio encoding
-        audioConfig: {audioEncoding: 'MP3'},
-    };
-
-// Performs the Text-to-Speech request
-    const [response] = await client.synthesizeSpeech(request);
-// Write the binary audio content to a local file
-    const writeFile = util.promisify(fs.writeFile);
-    await writeFile('output.mp3', response.audioContent, 'binary');
-    console.log('Audio content written to file: ');
+        // Write the binary audio content to a local file
+        const writeFile = util.promisify(fs.writeFile);
+        await writeFile('output.mp3', response.audioContent, 'binary');
+        console.log('Audio content written to file: ');
+    }
 }
 
-testSynthetize();
+module.exports = CloudTTS;
+
+
